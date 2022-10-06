e73879f2e0994fe5857909b4ef2375b7e9fb38d1
MuserRaven <99155887+MuserRaven@users.noreply.github.com>
2022-10-06 16:53:35 -0400

    Update Simulated_Spine_response_data.m

--- Simulated_Spine_response_data.m
+++ Simulated_Spine_response_data.m
@@ -8,12 +8,13 @@
 
 %% Perfectly reliable Case( both mu and sigma are fixed) And not reliable random cases
 %
+figure
 %Set the condition for data generator
-perfect = 0;
+perfect = 1;
 stimulate = struct;
 success = 0; 
 trialsize = 20;
-multiple_peaks = 0; % how shifty each trials peaks are 
+multiple_peaks = 0;
 
 
 for j = 1:6
@@ -22,48 +23,54 @@ type = 'perfect';
 
 gaus = @(x,mu,sig,amp,vo)amp*exp(-(((x-mu).^2)/(2*sig.^2)))+vo;
 
-if success == 0 && perfect == 1 
-  index = 0; normalization = 0;
-  amp = normrnd(1,3,trialsize);
-  vo = 0; 
-  x = linspace(0,26,26);
-  mu = normrnd(15,0.3,trialsize);
-  sig = 3*ones(trialsize,1);
-  noise = 10; noisetype = 'sinusoid'; frequency = 1;
+if perfect == 1 
 
-elseif success == 1 && perfect == 1
-  index = 0; normalization = 0;
-  amp = normrnd(26,3,trialsize);
-  vo = 0; 
-  x = linspace(0,26,26);
-  mu = normrnd(15,0.3,trialsize);
-  sig = 3*rand(trialsize,1);
-  noise = 10; noisetype = 'sinusoid'; frequency = 1;
+  if success == 0 
+    index = 0; normalization = 1;
+    amp = normrnd(1,3,trialsize);
+    vo = 0; 
+    x = linspace(0,26,26);
+    mu = normrnd(15,0.3,trialsize);
+    sig = 3*ones(trialsize,1);
+    noise = 10; noisetype = 'sinusoid'; frequency = 1;
 
-elseif perfect == 0 && multiple_peaks == 0
-  index = 0; normalization = 0;
-  %amp = normrnd(26,5,8);
-  amp = 2.6*chi2rnd(2,[trialsize,1]); % 2 degree of freedom chi2 distribution 
-  vo = 0; 
-  x = linspace(0,26,26);
-  mu = normrnd(15,5,trialsize); % set the mu to be rand around 15
-  sig = normrnd(2*rand(1,1),3,trialsize);
-  noise = 50; noisetype = 'sinusoid'; frequency = 0.1;
+  elseif success == 1 
+    index = 0; normalization = 1;
+    amp = normrnd(26,3,trialsize);
+    vo = 0; 
+    x = linspace(0,26,26);
+    mu = normrnd(15,0.3,trialsize);
+    sig = 3*rand(trialsize,1);
+    noise = 10; noisetype = 'sinusoid'; frequency = 1;
+  end
 
-elseif perfect == 0 && multiple_peaks == 1
-  index = 1 ; % set the index = 1 to initialize the term. 
-  %amp = normrnd(26,5,8);
-  normalization = 2; % set the normalization = 2 to initialize the term.
-  amp = 2.6*chi2rnd(2,[trialsize,1]); % 2 degree of freedom chi2 distribution 
-  vo = 0; 
-  x = linspace(0,26,26);
-  mu = normrnd(15,5,trialsize); % std of mu = 5
-  sig = normrnd(2*rand(1,1),3,trialsize);
-  noise = 50; noisetype = 'sinusoid'; frequency = 0.1;
-  amp2 = 2.6*chi2rnd(2,[trialsize,1]); % amplitude for the second peak
-  mu2 = normrnd(15,5,trialsize); % set the mu to be rand around 15
-  sig2 = normrnd(2*rand(1,1),3,trialsize); 
-    
+elseif perfect == 0 
+
+  if multiple_peaks == 0
+    index = 0; normalization = 1;
+    %amp = normrnd(26,5,8);
+    amp = 2.6*chi2rnd(2,[trialsize,1]); % 2 degree of freedom chi2 distribution 
+    vo = 0; 
+    x = linspace(0,26,26);
+    mu = normrnd(15,5,trialsize); % set the mu to be rand around 15
+    sig = 3*rand(trialsize,1);
+    noise = 50; noisetype = 'sinusoid'; frequency = 0.1;
+
+  elseif multiple_peaks == 1
+    index = 1 ; % set the index = 1 to initialize the term. 
+    %amp = normrnd(26,5,8);
+    normalization = 2; % set the normalization = 2 to initialize the term.
+    amp = 2.6*chi2rnd(2,[trialsize,1]); % 2 degree of freedom chi2 distribution 
+    vo = 0; 
+    x = linspace(0,26,26);
+    mu = normrnd(15,5,trialsize); % std of mu = 5
+    sig = 3*rand(trialsize,1);
+    noise = 50; noisetype = 'sinusoid'; frequency = 0.1;
+    amp2 = 2.6*chi2rnd(2,[trialsize,1]); % amplitude for the second peak
+    mu2 = normrnd(15,5,trialsize); % set the mu to be rand around 15
+    sig2 = 3*rand(trialsize,1); % standard deviation of the second peak 
+  end   
+end
 
 for i = 1:trialsize
   y = (gaus(x,mu(i),sig(i),amp(i),vo) + index*gaus(x,mu2(i),sig2(i),amp2(i),vo) + noise*sin(frequency*rand(1,26)))/normalization;
@@ -88,7 +95,7 @@ end
 
 end
 end
-end
+
 
 % Label simulated datasets as 'reliable' or 'unreliable'
 % reliable can have two states 1. reliable failure(non-preferred stimulus)
@@ -102,6 +109,8 @@ end
 % 6. Klustering properties of peaks values.
 % 7. Deviation within a cluster
 % 8. 
+
+
 
 
 
