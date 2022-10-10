74871fd4d92edf28c276549d1690b31633b5cd68
MuserRaven <99155887+MuserRaven@users.noreply.github.com>
2022-10-10 16:13:25 -0400

    Update ChatterjeeCorr.m

--- ChatterjeeCorr.m
+++ ChatterjeeCorr.m
@@ -1,5 +1,5 @@
 function [corrresponse] = ChatterjeeCorr(data)
-% Calculate chatterjee correlation ;
+% Calculate chatterjee correlation ( xlabel is nudge to the right by a random small number between 0.00001:0.0002 ;
 
 datalength = length(data);
 trialnum = length(data(:,1));
@@ -14,19 +14,21 @@ y = data(n,:);
   end
 end
 
-pair = sortrows(pair,1); 
-[~,p] = sort(pair(:,2),'ascend');
-r = 1:length(pair(:,2));
-r(p) = r; yrank = r';
+%dt = repmat(0.000000001*rand(20,1),8,1);  % initiate smal nudge by a random nuber
+%dt = dt(randperm(length(dt)));
+pair(:,1) = pair(:,1); %+ %dt;
+test = sortrows(pair,1);
+[~,ii]=sort(test(:,2),'ascend');
+[~,yrank]=sort(ii);
 
 sumy = nan(datalength*trialnum-1,1);
 for i = 1:datalength*trialnum-1
 sumy(i) = abs(yrank(i+1)-yrank(i));
 end
-
-%disp 'ChatterjCorrelation = ' 
 corrresponse = 1-(((3*sum(sumy))/((datalength*trialnum)^2 -1)));
 
 
+
+% TRY NUDGING X BY 0.0001 FOR ALL X TO INCREASE THE NUMBER OF RANK 
 
 %
