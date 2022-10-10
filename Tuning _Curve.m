6a0d50faf7b5bdaca64f387616d10a2c556c7188
MuserRaven <99155887+MuserRaven@users.noreply.github.com>
2022-10-10 16:14:31 -0400

    Update Tuning _Curve.m

--- Tuning _Curve.m
+++ Tuning _Curve.m
@@ -1,37 +1,36 @@
 Spike_triggered_average(dataStruct.dendrite.dff,100,8)
-%% Tuning Curve
+%% Response Visualization And Correlation/P estimate
+rng('default')
 
-t = 1:1:3e2;
-f = exp(-t./8);%
-for h = 1:length(dataStruct.spine)
-spinetrimean = nan(33,26);
-  for j = 1:33 % average across stimuli
-
-    spinesim = zeros(26,1);
-    for i = 1:8 % average across trials
-      
-      spinesim = spinesim + squeeze(dataStruct.spine(h).responses(j,i,:));
-  
-      % plot dff of stimulus 1 over all trials 
+spinenum = 3;
+P = nan(length(dataStruct.spine(1).responses),length(dataStruct.spine));
+for j = 7
+    figure
+    for i = 1:22
+        response = squeeze(dataStruct.spine(j).responses(i,:,:));
+        count = max(response,[],2)>1.6;
+        count = sum(count)/8; err = std(response,[],1);
+        
+        ChatterjeeCorr(response)
+        aver = (ChatterjeeCorr(response)+count)/2;
+        subplot(2,11,i); 
+        plot(1:20,response','k',LineWidth=1);ylim([0 6]);axis off; hold on;
+        scatter(1:20,mean(response,1),'b','filled'); hold on; errorbar(1:20,mean(response,1),err,'r'); % plot the mean of all trials in each timestamp
+        title({['Corr = ', num2str(ChatterjeeCorr(response))],['P-Thres = ',num2str(count)], ...
+            ['Aver = ',num2str(aver)]})
+          
     end
-    spinesim = spinesim/8;
-    spinetrimean(j,:) = spinesim;
-  end
-
-% spinestrimean store the spine's response to all 33 stimuli in a length 26
-% window
-figure
-plot(mean(spinetrimean(:,1:6),2))
 end
-%% Extract spine of interest
-% Say we are interest in spine 7 perferred stimulus = 14 dffmax = 0.5871
-testdata = squeeze(dataStruct.spine(7).responses(14,:,:));
-testdata = reshape(testdata,[8*26,1]);
-histogram(testdata,208,"EdgeColor","r")
-hold on 
-testdata = squeeze(dataStruct.spine(7).responses(4,:,:));
-testdata = reshape(testdata,[8*26,1]);
-histogram(testdata,208,"EdgeColor","b")
-%% T
 
-plot(deconv(dataStruct.spine(1).dffStimTrials(1,:),f))
+% Should I be assuming that the spikes of non-preferred direction are
+% uncorrelated ?
+% MAYBE I should combine threshold and Chatterjee correlation 
+% threshold probability == Number of trials that pass threshold/total number of trials
+% If Chatterjee > 0.2 and threshold probability > 0.5 -> consistent success
+% If Chatterjee < 0.2 and threshold probability == 0 -> consistent failure
+% If Chatterjee < 0.2 and threshold probability > 0 -> inconsistent 
+% The above is all I have now for the classification
+
+% But, we can also plot the peaks distribution, std within peaks, k-means, DBSCAN, mean, and all sorts of parameters. 
+% 
+% But I would like to first try out the threshold + chatterjee to see how well it works!
