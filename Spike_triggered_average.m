49f6e03fd08659f012a04f1584c321b22280416d
MuserRaven <99155887+MuserRaven@users.noreply.github.com>
2022-10-11 10:19:59 -0400

    Update Spike_triggered_average.m

--- Spike_triggered_average.m
+++ Spike_triggered_average.m
@@ -1,17 +1,17 @@
-function [avg, nEvs] = Spike_triggered_average(stim, windowSize)
+function [evIdx] = Spike_triggered_average(stim, windowSize,frame_decay)
 
-% magic number for spike
 t = 1:1:3e2;
-f = exp(-t./frame_decay);%30 frame decay
+f = exp(-t./frame_decay);%
 stim = deconv(medfilt1(stim,3),f);
-threshold = 3*std(stim);
+threshold = 5*std(stim);
 resp = stim>threshold;
 % response
-subplot(2,1,1), plot(stim)
+%figure
+%subplot(2,1,1), plot(stim)
 line([1, length(stim)], [threshold, threshold], 'LineStyle', '--', ...
     'color', 'k')
 ylabel('DFF'), title('Stimulus');
-subplot(2,1,2), plot(resp(1:length(stim)), 'r')
+%subplot(2,1,2), plot(resp(1:length(stim)), 'r')
 ylabel('Spikes'), title('Response')
 xlabel('Time')
 
@@ -19,25 +19,26 @@ xlabel('Time')
 resp(1:windowSize)=0;
 
 nEvs = sum(resp); % the number of responses
-disp 'number of response nEvs', num2str(nEvs)
 evIdx = find(resp); % the index of responses
 
 %Save the average in the variable avg, so preallocate it before the for loop.
 avg = zeros(1,windowSize);
-for w = 1:nEvs
+  for w = 1:nEvs
     % Find the indexes of the time window preceding the event
     wIdx = evIdx(w)-windowSize : evIdx(w)-1;
     % Add the dffdendrite from this window to the average
     avg = avg + stim(wIdx);
-end
+  end
 
 avg=avg./sum(resp);
 
-figure
-plot(avg)
-title(['Average of ', num2str(nEvs),  'windows'])
-xlabel('Time')
-ylabel('DFF')
-
-
+%figure
+%plot(avg)
+%hold on 
+%plot(movmean(avg,10),'-.')
+%title(['Average of ', num2str(nEvs),  'windows'])
+%xlabel('Time')
+%ylabel('DFF')
+%figure
+%histogram(stim,length(stim))
 end
