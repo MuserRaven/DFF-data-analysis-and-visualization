08d27acac24a8645471339b90658d450f00ef827
MuserRaven <99155887+MuserRaven@users.noreply.github.com>
2022-10-11 15:37:50 -0400

    Update Sample_test_for_simulatd_data.m

--- Sample_test_for_simulatd_data.m
+++ Sample_test_for_simulatd_data.m
@@ -2,19 +2,22 @@
 %  1 = reliable success
 %  2 = reliable failure
 %  3 = unreliable
+re = {'reliable_success', 'reliable_failure', 'unreliable_singlepeak', 'unreliable_multipeak', 'unreliable_type1'};
 store = nan(8,26);
+accuracy = nan(5,2);
+for h = 1:5
 prediction = nan(1000,1);
 for j = 1:1000
 for i  = 1:8
-store(i,:) = simulateData.unreliable_type1.(['stimulus',num2str(j)])(i).trial_trace;
+store(i,:) = simulateData.(char(re(h))).(['stimulus',num2str(j)])(i).trial_trace;
 %plot(1:26, simulateData.unreliable_multipeak.(['stimulus',num2str(j)])(i).trial_trace,'k')
 hold on 
 end
 ChatterjeeCorr(store);
-count = max(store,[],2)>25;
+count = max(store,[],2)>18;
 count = sum(count)/8;
 aver = (count + ChatterjeeCorr(store))/2;
-if aver >= 0.4 && count >= 0.7
+if aver >= 0.4 && count >= 0.625
     disp 'reliable_success'
     prediction(j) = 1;
 elseif count == 0 
@@ -26,11 +29,25 @@ else
 
 end
 end
-prediction = rmmissing(prediction);
-accuracy = length(find(prediction == 3))/length(prediction)
-%%
-figure
-plot(store')
+
+if strcmp(char(re(h)),'reliable_success')
+accuracy(1,1) = length(find(prediction == 1))/length(prediction);
+
+elseif strcmp(char(re(h)),'reliable_failure')
+accuracy(2,1) = length(find(prediction == 2))/length(prediction);
+
+elseif strcmp(char(re(h)),'unreliable_singlepeak')
+accuracy(3,1) = length(find(prediction == 3))/length(prediction);
+
+elseif strcmp(char(re(h)),'unreliable_multipeak')
+accuracy(4,1) = length(find(prediction == 3))/length(prediction);
+
+elseif strcmp(char(re(h)),'unreliable_type1')
+accuracy(5,1) = length(find(prediction == 3))/length(prediction);
+
+
+end
+end
 
 
 
