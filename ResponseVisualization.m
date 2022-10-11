e4f61c8d6fe668b327aec8e4acf84e844030e0d7
MuserRaven <99155887+MuserRaven@users.noreply.github.com>
2022-10-11 13:45:34 -0400

    Update ResponseVisualization.m

--- ResponseVisualization.m
+++ ResponseVisualization.m
@@ -4,22 +4,24 @@ rng('default')
 
 spinenum = 3;
 P = nan(length(dataStruct.spine(1).responses),length(dataStruct.spine));
-for j = 7
+threshold = 1.4; % set the spike detecting threshold
+for j = 1:40
+
     figure
-    for i = 1:22
-        response = squeeze(dataStruct.spine(j).responses(i,:,:));
-        count = max(response,[],2)>1.6;
+    for i = 1:15
+        response = squeeze(dataStruct.spine(j).responses_combos(i,:,:));
+        count = max(response,[],2) > threshold;
         count = sum(count)/8; err = std(response,[],1);
-        
         ChatterjeeCorr(response)
         aver = (ChatterjeeCorr(response)+count)/2;
-        subplot(2,11,i); 
+        subplot(2,8,i); 
         plot(1:20,response','k',LineWidth=1);ylim([0 6]);axis off; hold on;
-        scatter(1:20,mean(response,1),'b','filled'); hold on; errorbar(1:20,mean(response,1),err,'r'); % plot the mean of all trials in each timestamp
+        scatter(1:20,mean(response,1),'b','filled'); hold on; %errorbar(1:20,mean(response,1),err,'r'); % plot the mean of all trials in each timestamp
         title({['Corr = ', num2str(ChatterjeeCorr(response))],['P-Thres = ',num2str(count)], ...
             ['Aver = ',num2str(aver)]})
-          
+        
     end
+    sgtitle(['Spine Number', num2str(j)])
 end
 
 % Should I be assuming that the spikes of non-preferred direction are
@@ -34,3 +36,4 @@ end
 % But, we can also plot the peaks distribution, std within peaks, k-means, DBSCAN, mean, and all sorts of parameters. 
 % 
 % But I would like to first try out the threshold + chatterjee to see how well it works!
+
